6af4c84dea9c440324c55454628360eb76a078fd
Paul Sausage <luc.pitipuis@gmail.com>
2016-01-11 15:47:25 +0900

    Qdd post and user and link the two of them

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
   before_action :authenticate_user!
   def home
+    #@posts = @Post.paginate(page: params[:page])
+    @post = current_user.posts.build
+    @posts = current_user.posts.all
   end
 
   def help
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.paginate(page: params[:page])
+  end 
+  
+end
